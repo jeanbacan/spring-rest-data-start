06f4f6d77499e9406910ab3fef4c2605ff2bb340
Jean Marcel Moreno Bacan <jean.moreno@gvt.com.br>
2018-02-22 15:48:33 -0300

    First commit

---
+++ src/main/java/br/com/bacan/testlambda/TestLambdaApplication.java
@@ -0,0 +1,12 @@
+package br.com.bacan.testlambda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestLambdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestLambdaApplication.class, args);
+	}
+}
